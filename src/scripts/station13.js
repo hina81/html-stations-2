8196c680e71c5c30a58e860991ab6638fbf7a71e
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-03-29 22:48:19 +0900

    owari

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  if (check.checked) {
+    console.log("checked");
+    text.style.backgroundColor = "red";
+  } else {
+    console.log("unchecked");
+    text.style.backgroundColor = "white";
+  }
 }
---
+++ src/scripts/station14.js
@@ -0,0 +1,3 @@
+function setAlert() {
+    alert("ECMAScript");
+}
